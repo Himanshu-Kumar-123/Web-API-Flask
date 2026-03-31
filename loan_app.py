9f7270a26a589a5262180b673ca653a5aaa84a9c
himkumar <himkumar@nvidia.com>
2026-04-01 00:21:19 +0530

    Enhance predict function to process JSON input for loan approval predictions

--- loan_app.py
+++ loan_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import pickle
 
 with open("classifier.pkl", "rb") as f:
@@ -17,4 +17,28 @@ def predict():
         return "I will predict the loan approval status for you"
     else:
         # Post request along with the input data to predict the loan approval status
-        return model.predict(inputs)
+        loan_req = request.get_json()
+        
+        if loan_req['Gender'] == "Male":
+            Gender = 0
+        else:
+            Gender = 1
+
+        if loan_req['Married'] == "No":
+            Married = 0
+        else:
+            Married = 1
+        
+        ApplicantIncome = loan_req['ApplicantIncome']
+        LoanAmount = loan_req['LoanAmount']
+        Credit_History= loan_req['Credit_History']           
+
+        input_data = [Gender, Married, ApplicantIncome, LoanAmount, Credit_History]
+        result = model.predict([input_data])
+        
+        if result == 0:
+            pred = "Rejected"
+        else:
+            pred = "Approved"
+
+        return {"loan_approval_status":pred}
